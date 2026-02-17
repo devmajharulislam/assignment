b12bbde6a4da3ca0ac698e5532ab2d3cb037ff53
majharulislam <dev.majharulislam@gmail.com>
2026-02-17 12:02:06 +0600

    finished assignment

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,6 @@ import "./globals.css";
 import Footer from "@/components/ui/Footer";
 import AuthGuard from "@/components/AuthGuard";
 import Navbar from "@/components/ui/NavBar";
-
-
-
 export default function RootLayout({
   children,
 }: {
@@ -13,7 +10,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body >
-
         <Navbar/>
         <AuthGuard>{children}</AuthGuard>
       <Footer/>
