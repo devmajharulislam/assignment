3b152b8e9dfd1379cfdb624f050341eb9ac2368d
majharulislam <dev.majharulislam@gmail.com>
2026-02-24 11:01:38 +0600

    added checkout and order page

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,23 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "via.placeholder.com",
-      },
-      {
-        protocol: "http",
-        hostname: "via.placeholder.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn-nextshop.prospectbdltd.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn-staging-nextshop.prospectbdltd.com",
-      },
-    ],
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "cdn-staging-nextshop.prospectbdltd.com",
+            },
+            {
+                protocol: "https",
+                hostname: "staging-nextshop-backend.prospectbdltd.com",
+            },
+            {
+                // Needed because the CDN redirects to this for placeholder/test images
+                protocol: "https",
+                hostname: "via.placeholder.com",
+            },
+        ],
+    },
+    unoptimized: true,
 };
 
 export default nextConfig;
